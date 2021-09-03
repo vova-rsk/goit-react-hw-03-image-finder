a923f958d7690ee15c062818ca139f2e97c08cfd
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-03 21:00:49 +0300

    changed export type

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,8 @@
-export const ScrollTo = () => {
+const scrollTo = () => {
   window.scrollTo({
     top: document.documentElement.scrollHeight,
     behavior: 'smooth',
   });
 };
+
+export default scrollTo;
