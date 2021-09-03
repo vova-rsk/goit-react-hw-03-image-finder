23e1f3135bafa73b8d6ce6edd43c81225ac0e35c
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-03 19:54:55 +0300

    added working search & gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,66 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+import Searchbar from './components/Searchbar';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Button from './components/Button';
+import fetchImages from './services/pixabay-api';
 
-class App extends PureComponent {
-  state = {};
+class App extends Component {
+  state = {
+    isLastPage: false,
+    page: 1,
+    query: '',
+    searchResult: [],
+  };
+
+  fetchQueryUpdate = query => {
+    this.setState({ query });
+    this.resetPage();
+  };
+
+  resetPage = () => {
+    this.setState({ page: 1 });
+  };
+
+  handleIncrementPage = () => {
+    this.setState(prevState => ({ ...prevState, page: prevState.page + 1 }));
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    const isQueryChanged = prevState.query !== query;
+    const isPageIncreased = prevState.page < page;
+
+    if (isQueryChanged || isPageIncreased) {
+      fetchImages(query, page)
+        .then(({ hits, totalHits }) =>
+          this.setState(prevState => {
+            return {
+              ...prevState,
+              searchResult: isQueryChanged
+                ? hits
+                : [...prevState.searchResult, ...hits],
+              isLastPage: isQueryChanged
+                ? hits.length >= totalHits
+                : [...prevState.searchResult, ...hits].length >= totalHits,
+            };
+          }),
+        )
+        .catch(error => console.error(error.message));
+      // .finally();
+    }
+  }
 
   render() {
-    return <div>KOKOKO</div>;
+    const { searchResult, isLastPage } = this.state;
+    return (
+      <>
+        <Searchbar fetchQueryUpdate={this.fetchQueryUpdate} />
+        {searchResult && <ImageGallery galleryItems={searchResult} />}
+        {!isLastPage && (
+          <Button handleIncrementPage={this.handleIncrementPage} />
+        )}
+      </>
+    );
   }
 }
 
