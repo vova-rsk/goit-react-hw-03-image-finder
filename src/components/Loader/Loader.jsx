ce62abee8b0bb099f5c46032a07a15c576e619ed
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-03 22:32:17 +0300

    changed settings

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Container from './Loader.styled';
 
 const LoaderSpinner = () => {
   const options = {
-    type: 'Puff',
-    color: '#00BFFF',
+    type: 'ThreeDots',
+    color: '#eb4034',
     height: 100,
     width: 100,
-    timeout: 3000,
   };
-  return <Loader {...options} />;
+  return (
+    <Container>
+      <Loader {...options} />
+    </Container>
+  );
 };
 
 export default LoaderSpinner;
