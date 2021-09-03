77cd0fdd23679215f24d396c9f9158571da05e2a
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-03 19:53:50 +0300

    added working image-gallery component

--- src/components/ImageGallery/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery/ImageGallery.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ImageGalleryItem from '../ImageGalleryItem';
 
-const ImageGallery = props => {
+const ImageGallery = ({ galleryItems }) => {
   return (
     <ul className="ImageGallery">
-      {/* <!-- Набор <li> с изображениями --> */}
+      {galleryItems.map(({ id, webformatURL, tags }, idx) => (
+        <ImageGalleryItem
+          // key={id}
+          key={idx}
+          src={webformatURL}
+          alt={tags}
+        />
+      ))}
     </ul>
   );
 };
 
 export default ImageGallery;
 
-ImageGallery.propTypes = {};
+ImageGallery.propTypes = {
+  galleryItems: PropTypes.arrayOf(PropTypes.object),
+};
