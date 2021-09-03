92380658839fe9967f718bc2b8c5ac940aa888f0
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-03 22:31:49 +0300

    changed spinner position

---
+++ src/components/Loader/Loader.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export default Container;
