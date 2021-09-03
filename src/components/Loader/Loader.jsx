8d66ae9e105352fa109fb3d6e182f318d9640741
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-03 21:30:19 +0300

    added Loader component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,3 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
+const LoaderSpinner = () => {
+  const options = {
+    type: 'Puff',
+    color: '#00BFFF',
+    height: 100,
+    width: 100,
+    timeout: 3000,
+  };
+  return <Loader {...options} />;
+};
+
+export default LoaderSpinner;
